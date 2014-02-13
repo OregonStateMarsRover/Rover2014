b852b40a007a3e1b0f51ec05e58c97e709f3ce58
Erich Merrill <merriler@engr.oregonstate.edu>
2014-02-12 22:11:21 -0800

    Starting to separate display from internal representation

--- software/ros-packages/roscv/scripts/obs.py
+++ software/ros-packages/roscv/scripts/obs.py
@@ -11,7 +11,7 @@ import math
 WIDTH=640/2
 HEIGHT=480/2
 DEPTH_SCALE=30
-MIN_H = 1.0
+MIN_H = 1.5
 MAX_H = 2.0
 THETA = math.pi/4.0
 
@@ -20,6 +20,9 @@ class obstacle_detector:
 		self.disp_callback = rospy.Subscriber("/my_stereo/disparity", DisparityImage, self.callback_disp)
 		cv2.namedWindow("depth", cv2.WINDOW_AUTOSIZE)
 		cv2.namedWindow("obstacle", cv2.WINDOW_AUTOSIZE)
+		#cv2.namedWindow("1", cv2.WINDOW_AUTOSIZE)
+		cv2.namedWindow("2", cv2.WINDOW_AUTOSIZE)
+		cv2.namedWindow("3", cv2.WINDOW_AUTOSIZE)
 
 	def callback_disp(self, data):
 		focal = data.f
@@ -30,14 +33,22 @@ class obstacle_detector:
 		dist = fb / disp_np
 		dist = cv2.resize(dist, (WIDTH, HEIGHT))
 
-		scale = dist / (dist.mean() * 3)
+		sf = dist.mean() * 3.0
+		scale = dist / sf
 		cv2.imshow("depth", scale)
 		obs = np.zeros((HEIGHT,WIDTH), np.uint8)
 
 		#self.filter_dist(dist, obs, 5, 8)
 		self.find_obstacles(dist, obs)
-		cv2.imshow("obstacle", obs)
-		#cv2.imwrite("out.png", obs)
+
+		scale_obs = obs.copy()
+		scale_obs = scale_obs / sf
+		cv2.imshow("obstacle", scale_obs)
+
+		
+		o1 = np.zeros((HEIGHT,WIDTH), np.bool)
+		o2 = np.zeros((HEIGHT,WIDTH), np.bool)
+		o3 = np.zeros((HEIGHT,WIDTH), np.bool)
 		cv2.waitKey(1)
 
 	def filter_dist(self, depth, obs, min_d, max_d):
@@ -47,13 +58,12 @@ class obstacle_detector:
 				obs[x,y] = 255
 
 	def find_obstacles(self, depth, obs):
-		print "Start"
 		c = [x for x in np.ndindex(depth.shape)]
 		c.reverse()
 		for y, x in c:
 			d = depth[y,x]
 			if d < 0: continue
-			if obs[y,x] > 255: continue
+			if obs[y,x] > 0: continue
 
 			scale = self.depth_scale(d)
 			min_row = y - int(max(MIN_H * scale, 1.0))
@@ -69,17 +79,21 @@ class obstacle_detector:
 				dx = int(math.tan(THETA) * (y-py))
 				min_col = x - dx
 				max_col = x + dx
+				min_col = max(0, min_col)
+				max_col = min(WIDTH-1, max_col)
 				for px in range(min_col, max_col):
 					pd = depth[py, px]
 					if pd < 0: continue
 					dz = dx / (scale * DEPTH_SCALE)
 					if d - dz < pd < d + dz:
 						obstacle = True
-						#obs[py, px] = 255
-						break
-			if obstacle: obs[y,x] = 255
-			else: obs[y,x] = 100
-		print "Done"
+						obs[py, px] = d
+			if obstacle: obs[y,x] = d
+	
+	def filter(self, obs, min_, max_, out):
+		for y, x in np.ndindex(depth.shape):
+			if min_ < obs[y,x] < max_:
+				out[y,x] = 1 
 
 	def depth_scale(self, depth):
 		return 5.0
