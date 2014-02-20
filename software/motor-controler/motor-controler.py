7fd5d02f66d5d90a70529a0521ee98a58cbbc9c2
Cody Ray Freeman Hoeft (haroldhues) <hoeftc@onid.oregonstate.edu>
2014-02-19 18:27:06 -0800

    Improved Motor-controler.py

--- software/motor-controler/motor-controler.py
+++ software/motor-controler/motor-controler.py
@@ -1,6 +1,8 @@
 import itertools
 
 def main():
-    publisher_data = ["".join(x) for _, x in itertools.groupby("dfsd98sd8f68as7df56", key=str.isdigit)]
-    print(publisher_data[1])
+    publisher_string = "s2f2r90f35s1"
+    publisher_data = ["".join(x) for _, x in itertools.groupby(publisher_string, key=str.isdigit)]
+    #Publisher Data is an array of alternating chars and integers
+    print(publisher_data)
 main()
