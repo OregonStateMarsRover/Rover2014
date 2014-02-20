2e9e1ea294feac3f23efe73b6c877c78725468e1
Cody Ray Hoeft <hoeftc@onid.oregonstate.edu>
2014-02-20 00:06:33 -0800

    I added Classes for each of the controlers, I also created the general structure for the code, I used a created a switch statement to handle the different flags, works with test string. Currently only outputs statements to the screen

--- software/motor-controler/motor-controler.py
+++ software/motor-controler/motor-controler.py
@@ -1,8 +1,101 @@
-import itertools
+import itertools #Used to break apart string
 
-def main():
-    publisher_string = "s2f2r90f35s1"
+DEFAULTSPEED = 20 #Should be a integer
+
+#Specific Class Structure
+class motor_control_class:
+    def __init__(self, port):
+        self.port = port
+        self.speed = DEFAULTSPEED
+    
+    def setspeed(self, raw_speed):
+        self.speed = 10 * int(raw_speed)
+        print("Set Speed: " + raw_speed)
+
+    def __waitr(self):
+        #Wait for R response from controller
+        #use self.port
+        pass
+
+    def sendstatus(self, status):
+        #not to sure about this one
+        pass
+
+    def forward(self, raw_distance):
+        distance = 100 * int(raw_distance)
+        self.__waitr()
+        print("Distance: " + raw_distance)
+
+    def backward(self, raw_distance):
+        distance = 100 * int(raw_distance)
+        self.__waitr()
+        print("Distance Backward: " + raw_distance)
+
+    def rotate(self, raw_degrees):
+        degrees = int(raw_degrees)
+        self.__waitr()
+        print("Rotate: " + raw_degrees)
+        
+class arm_control_class:
+    def __init__(self, port):
+        self.port = port
+        self.speed = DEFAULTSPEED
+    
+    def setspeed(self, raw_speed):
+        self.speed = 10 * int(raw_speed)
+        print("Set Speed: " + raw_speed)
+
+    def __waitr(self):
+        #Wait for R response from controller
+        #use self.port
+        pass
+
+    def sendstatus(self, status):
+        #not to sure about this one
+        pass
+
+    def base_rotate(self, raw_degrees):
+        degrees = 100 * int(raw_degrees)
+        self.__waitr()
+        print("Rotate Base to " + raw_degrees)
+
+def registerdevices():
+    dictionary = dict()
+    dictionary['motor'] = "/dev/ptt2/"
+    dictionary['arm'] = "/dev/ptt5/"
+    return dictionary
+
+#Hook for pause function of robot
+def checkforpause():
+    pass
+
+#Gets the new feed 
+def get_published():
+    publisher_string = "f100s10r45b500d30f5"
     publisher_data = ["".join(x) for _, x in itertools.groupby(publisher_string, key=str.isdigit)]
     #Publisher Data is an array of alternating chars and integers
-    print(publisher_data)
+    return publisher_data
+
+def main():
+    ports = registerdevices()
+    body = motor_control_class(ports['motor'])
+    arm = arm_control_class(ports['arm'])
+    repeat = True #Just for now
+    while(repeat):
+        path = get_published()
+        body.sendstatus(1) #We are in the loop and busy
+        ##The Loop
+        #Only iterate through the odd numbers that should have a letter
+        for x in [x for x in range(len(path)) if not(x % 2)]:
+            #Exploits a dictionary to create a switch statement
+            {
+                'f': body.forward,
+                'b': body.backward,
+                's': body.setspeed,
+                'r': body.rotate,
+                'd': arm.base_rotate
+            } [path[x]] (path[x+1])
+        ##
+        repeat = False
+
 main()
