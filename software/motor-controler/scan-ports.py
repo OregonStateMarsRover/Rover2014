d531c761179a9c6667b10c572817f68eb6dcdacd
Cody Ray Freeman Hoeft (haroldhues) <hoeftc@onid.oregonstate.edu>
2014-02-21 17:08:18 -0800

    Added Alternative way for newest version of pyserial

--- software/motor-controler/scan-ports.py
+++ software/motor-controler/scan-ports.py
@@ -1,6 +1,7 @@
 #http://stackoverflow.com/questions/11303850/what-is-the-cross-platform-method-of-enumerating-serial-ports-in-python-includi
 #Sometimes Misses Virtual Serial Ports on Linux
 import serial
+from serial.tools import list_ports
 
 def scan():
    # scan for available ports. return a list of tuples (num, name)
@@ -16,3 +17,5 @@ def scan():
 
 print "Found ports:"
 for n,s in scan(): print "(%d) %s" % (n,s)
+# For latest version of pyserial:
+# list_ports
