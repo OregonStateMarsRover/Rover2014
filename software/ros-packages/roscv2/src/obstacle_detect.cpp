8a65d70c770b2b7a7ee79f54843f915e2fc51ec9
Erich Merrill <merriler@engr.oregonstate.edu>
2014-03-01 02:02:23 -0800

    pre-bedtime commit

--- software/ros-packages/roscv2/src/obstacle_detect.cpp
+++ software/ros-packages/roscv2/src/obstacle_detect.cpp
@@ -9,13 +9,17 @@ int main(int argc, char *argv[]) {
 	init_cv();
 
 	/* Subscribe */
+	/*
 	ros::Subscriber disparity_sub = n.subscribe("/my_stereo/disparity",
 	                                1, disparity_callback);
 	ros::Subscriber image_sub = n.subscribe("/my_stereo/left/image_rect_color",
 	                            1, image_callback);
+	*/
 
 	/* Spin */
 	while (ros::ok()) {
+		ROS_INFO("Loop!");
+		loop();
 		cv::waitKey(1);
 		ros::spinOnce();
 	}
@@ -25,32 +29,99 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void disparity_callback(const stereo_msgs::DisparityImage::ConstPtr& msg) {
-	//ROS_INFO("Disparity callback!");
-	cv_bridge::CvImagePtr cv_disp = cv_bridge::toCvCopy(msg->image,
-	                                "32FC1");
-	float focal = msg->f; float baseline = msg->T;
-	cv::Mat depth = (focal * baseline) / cv_disp->image; //Get depth map
+void loop() { 
+	/* Grab a pair of images */
+	sensor_msgs::Image::ConstPtr img_msg;
+	stereo_msgs::DisparityImage::ConstPtr disp_msg;
 
-	cv::Mat scaled_depth = depth / RANGE_MAX; //Scale it for display
-	cv::imshow(DEPTH_WINDOW, scaled_depth);
-}
+	get_images(img_msg, disp_msg);
 
-void image_callback(const sensor_msgs::Image::ConstPtr& msg) {
-	//ROS_INFO("Image callback!");
-	cv_bridge::CvImagePtr img = cv_bridge::toCvCopy(msg,
+	/* Display raw image */
+	cv_bridge::CvImagePtr img = cv_bridge::toCvCopy(img_msg,
 	                            sensor_msgs::image_encodings::BGR8);
 	cv::imshow(IMAGE_WINDOW, img->image);
+
+	/* Get disparity data */
+	cv_bridge::CvImagePtr disp = cv_bridge::toCvCopy(disp_msg->image,
+	                             "32FC1");
+	float focal = disp_msg->f; float base = disp_msg->T;
+
+	/* Generate depth image */
+	float min_depth = (focal * base) / disp_msg->min_disparity;
+	float max_depth = (focal * base) / disp_msg->max_disparity;
+	cv::Mat full_depth = (focal * base) / disp->image;
+	cv::Mat depth;
+	cv::resize(full_depth, depth, cv::Size(IMG_WIDTH, IMG_HEIGHT));
+
+	/* Display value-scaled depth image */
+	cv::Mat scaled_depth = depth / RANGE_MAX;
+	cv::imshow(DEPTH_WINDOW, scaled_depth);
+
+	/* Create empty obstacle map */
+	cv::Mat obstacle(IMG_HEIGHT, IMG_WIDTH, CV_32FC1, 0.0);
+
+	/* Find and display obstacles */
+	find_obstacles(depth, obstacle, min_depth, max_depth);
+	cv::imshow(OBS_WINDOW, obstacle);
+}
+
+void find_obstacles(const cv::Mat& depth, cv::Mat& obstacle, 
+                    float min, float max) {
+	for (int row = 0; row < depth.rows; row++) {
+		const float *d = (const float*)depth.ptr(row);
+		float *o = (float*)obstacle.ptr(row);
+		for (int col = 0; col < depth.cols; col++) {
+			float depth = d[col];
+			if (o[col] > 0) continue; /* Already an obstacle? Skip. */
+			if (depth < min || depth > max) continue; /* out of range */
+
+			/* Valid for examination */
+			float scale = get_depth_scale(depth);
+			int min_row = row - (int)std::max(MIN_H * scale, 1.0);
+			int max_row = row - (int)std::max(MAX_H * scale, 1.0);
+
+			/* Make sure we don't fall off the image! */
+			min_row = std::max(min_row, 0);
+			max_row = std::max(max_row, 0);
+
+			/* TODO Trade accuracy for speedup?? */
+			max_row = std::max(max_row, min_row-5);
+
+			/* Loop over relevant image rows to search for obstacle */
+			bool obstacle = false;
+			for(int subrow = max_row; subrow >= min_row; subrow--) {
+				int dx = (int)(tan(THETA) * row-subrow);
+				 /* TODO TODO Finish */
+			}
+		}
+	}
+}
+
+/* TODO???? */
+float get_depth_scale(float depth) {
+	float focal_length = 600.0;
+	float scale = focal_length / depth;
+	return scale;
+}
+
+void get_images(sensor_msgs::Image::ConstPtr& im,
+                stereo_msgs::DisparityImage::ConstPtr& dm) {
+	im = ros::topic::waitForMessage<sensor_msgs::Image>(
+	     "/my_stereo/left/image_rect_color");
+	dm = ros::topic::waitForMessage<stereo_msgs::DisparityImage>(
+	     "/my_stereo/disparity");
 }
 
 void init_cv() {
 	ROS_INFO("Starting CV");
 	cv::namedWindow(IMAGE_WINDOW, CV_WINDOW_AUTOSIZE);
 	cv::namedWindow(DEPTH_WINDOW, CV_WINDOW_AUTOSIZE);
+	cv::namedWindow(OBS_WINDOW, CV_WINDOW_AUTOSIZE);
 }
 
 void cleanup_cv() {
 	ROS_INFO("Destroying CV");
 	cv::destroyWindow(IMAGE_WINDOW);
 	cv::destroyWindow(DEPTH_WINDOW);
+	cv::destroyWindow(OBS_WINDOW);
 }
--- software/ros-packages/roscv2/src/obstacle_detect.hpp
+++ software/ros-packages/roscv2/src/obstacle_detect.hpp
@@ -1,19 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
 #include <ros/ros.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <stereo_msgs/DisparityImage.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
 
+/* Constants */
 #define IMAGE_WINDOW "Image"
 #define DEPTH_WINDOW "Distance"
+#define OBS_WINDOW "Obstacle"
+
+#define DOWNSCALE (2)
+
+#define IMG_WIDTH (640/DOWNSCALE)
+#define IMG_HEIGHT (480/DOWNSCALE)
 
 #define RANGE_MIN (0)
 #define RANGE_MAX (40)
 
+#define THETA (3.14159 / 6.0)
+#define MIN_H (0.10)
+#define MAX_H (0.15)
+
+
+/* Functions */
+void loop();
+void get_images(sensor_msgs::Image::ConstPtr&,
+                stereo_msgs::DisparityImage::ConstPtr&);
+void find_obstacles(const cv::Mat&, cv::Mat&, float, float);
+float get_depth_scale(float);
+
 void init_cv();
 void cleanup_cv();
 
-void disparity_callback(const stereo_msgs::DisparityImage::ConstPtr& msg);
-void image_callback(const sensor_msgs::Image::ConstPtr& msg);
+void disparity_callback(const stereo_msgs::DisparityImage::ConstPtr&);
+void image_callback(const sensor_msgs::Image::ConstPtr&);
