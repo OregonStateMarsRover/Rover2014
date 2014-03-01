e6b9ae6309e38cb70591f28555cfccd3abdb578b
Erich Merrill <merriler@engr.oregonstate.edu>
2014-02-28 20:34:20 -0800

    Got cv bridge working + subscribers up and running

--- software/ros-packages/roscv2/src/obstacle_detect.cpp
+++ software/ros-packages/roscv2/src/obstacle_detect.cpp
@@ -1,20 +1,47 @@
-#include "ros/ros.h"
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include "obstacle_detect.hpp"
 
 int main(int argc, char *argv[]) {
+	/* Init ROS */
 	ros::init(argc, argv, "obstacle_detect");
 	ros::NodeHandle n;
-	
 	ROS_INFO("obstacle_detect started");
 
-	cv::namedWindow("TestWindow", CV_WINDOW_AUTOSIZE);
+	init_cv();
 
+	/* Subscribe */
+	ros::Subscriber disparity_sub = n.subscribe("/my_stereo/disparity",
+	                                1, disparity_callback);
+	ros::Subscriber image_sub = n.subscribe("/my_stereo/left/image_rect_color",
+	                            1, image_callback);
+
+	/* Spin */
 	while (ros::ok()) {
 		cv::waitKey(1);
 		ros::spinOnce();
 	}
 
-	cv::destroyWindow("TestWindow");
+	cleanup_cv();
+
 	return 0;
+}
+
+void disparity_callback(const stereo_msgs::DisparityImage::ConstPtr& msg) {
+	ROS_INFO("Disparity callback!");
+}
+
+void image_callback(const sensor_msgs::Image::ConstPtr& msg) {
+	ROS_INFO("Image callback!");
+	cv_bridge::CvImagePtr img = cv_bridge::toCvCopy(msg,
+	                            sensor_msgs::image_encodings::BGR8);
+	cv::imshow(IMAGE_WINDOW, img->image);
+}
+
+void init_cv() {
+	ROS_INFO("Starting CV");
+	cv::namedWindow(IMAGE_WINDOW, CV_WINDOW_AUTOSIZE);
+}
+
+void cleanup_cv() {
+	ROS_INFO("Destroying CV");
+	cv::destroyWindow(IMAGE_WINDOW);
 }
---
+++ software/ros-packages/roscv2/src/obstacle_detect.hpp
@@ -0,0 +1,16 @@
+#include <ros/ros.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <stereo_msgs/DisparityImage.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+
+#define IMAGE_WINDOW "Image"
+#define DIST_WINDOW "Distance"
+
+void init_cv();
+void cleanup_cv();
+
+void disparity_callback(const stereo_msgs::DisparityImage::ConstPtr& msg);
+void image_callback(const sensor_msgs::Image::ConstPtr& msg);
