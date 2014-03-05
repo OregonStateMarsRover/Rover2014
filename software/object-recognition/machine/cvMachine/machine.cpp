8f2ea9ce9bcc360fd0de85f29cd0da72ac8c1578
Lane Beneman <ldb358@gmail.com>
2014-03-05 13:29:24 -0800

    started nn with opencv to see how it works performance wise

---
+++ software/object-recognition/machine/cvMachine/machine.cpp
@@ -0,0 +1,23 @@
+#define ISIZE 20
+#define LAYERSIZE 30
+
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#include <opencv2/opencv.hpp> //include all cpp
+
+using namespace cv;
+Mat create 
+
+
+void train_x2(){
+        
+}
+
+int main(){
+
+        return 0;
+}      
