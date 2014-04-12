8bc4f5123764ac256dedc9ac39c3d21563c0654f
Erich Merrill <merriler@engr.oregonstate.edu>
2014-04-11 19:14:30 -0700

    Restructured pathfinding

--- software/ros-packages/roscv2/src/path_finding.cpp
+++ software/ros-packages/roscv2/src/path_finding.cpp
@@ -1,30 +1,29 @@
-#include "obstacle_detect.hpp"
-#include <roscv2/grid.h>
-
-void gridCallback(const roscv2::Grid& msg);
-
-void gridCallback(const roscv2:Grid& msg) {
-    int grid[msg.x][msg.y];
-    int i,j,k;
-
-    for (i=0;i<x;i++) {
-        for (j=0;j<y;j++){
-            grid[i][k] = msg.array[k];
-            k++;
-        }
-    }
-
-}
-
-
+#include "path_finding.hpp"
 
 int main(int argc, char **argv) {
 
-    ros::init(argc, argv, "path finding");
+    ros::init(argc, argv, "path_finding");
     ros::NodeHandle pf;
     ROS_INFO("Path finding node online");
 
-    ros::Subscriber sub = pf.subscribe("obstacle_grid", 10, gridCallback);
+    ros::Subscriber sub = pf.subscribe("/obstacle_grid", 10, grid_callback);
 
-    ros::SpinOnce;
+	while (1) {
+		ros::spinOnce();
+	}
+}
+
+void grid_callback(const roscv2::Grid& msg) {
+	Grid grid = Grid::from_msg(msg);
+	print_grid(grid);
+}
+
+void print_grid(const Grid& grid) {
+	for (int j = 0; j < grid.height(); j++) {
+		for (int i = 0; i < grid.width(); i++) {
+			printf("%c ", grid[i][j] == 0 ? '-' : '#');
+		}
+		printf("\n");
+	}
+	printf("\n\n\n");
 }
