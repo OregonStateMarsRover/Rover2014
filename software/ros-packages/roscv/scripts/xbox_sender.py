dc85066223ce00fd7fc838f528eb2d21721d9dc0
Erich Merrill <merriler@engr.oregonstate.edu>
2014-04-27 15:21:58 -0700

    Changed host for xbox script

--- software/ros-packages/roscv/scripts/xbox_sender.py
+++ software/ros-packages/roscv/scripts/xbox_sender.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
 
     #set up socket
-    HOST = '192.168.0.50'# The remote host
+    HOST = '192.168.0.64'# The remote host
     PORT = 35800 # The same port as used by the server
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
