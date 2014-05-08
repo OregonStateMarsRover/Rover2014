152486531b91c6c0c1276f55947f77db7fb5f108
Erich Merrill <merriler@engr.oregonstate.edu>
2014-05-07 17:28:42 -0700

    Non-complete commit

--- software/ros-packages/brain/scripts/process_mgmt.py
+++ software/ros-packages/brain/scripts/process_mgmt.py
@@ -2,8 +2,20 @@
 import roslib
 import rospy
 import std_msgs
+import fcntl
+import os
+import time
 
 from subprocess import Popen, PIPE
+
+def nb_read(f):
+	fd = f.fileno()
+	fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+	fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+	try:
+		return f.read()
+	except:
+		return ""
 
 class ProcessManager:
 	def __init__(self):
@@ -22,6 +34,13 @@ class ProcessManager:
 		#Read from stereo_proc.stdout.readlines() ????
 		rospy.loginfo("Started stereo system")
 
+		while True:
+			time.sleep(1)
+			print "xxx"
+			sr = nb_read(self.stereo_proc.stdout)
+			if sr != "":
+				print sr
+
 	def start_obstacle(self):
 		obs_args = ['rosrun', 'roscv2', 'obstacle_detect']
 		self.obs_proc = Popen(obs_args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
@@ -31,6 +50,16 @@ class ProcessManager:
 		move_args = ['rosrun', 'roscv', 'pathfinding']
 		self.move_proc = Popen(move_args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 		rospy.loginfo("Started pathfinding")
+
+class StereoProcess:
+	def __init__(self):
+		self.cam_args = ['roslaunch', 'roscv', 'startCam.launch']
+		self.stero_args = ['roslaunch', 'roscv', 'startStereo.launch']
+		self.cam_running = False
+		self.stereo_running = False
+
+	def start(self):
+
 
 if __name__=="__main__":
 	rospy.init_node("process_manage")
