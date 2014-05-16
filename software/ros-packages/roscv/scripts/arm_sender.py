f85d8db77bf5d796b88572eb410354374ed847b2
ldb358 <ldb358@gmail.com>
2014-05-16 01:24:40 -0700

    arm sender

--- software/ros-packages/roscv/scripts/arm_sender.py
+++ software/ros-packages/roscv/scripts/arm_sender.py
@@ -25,41 +25,55 @@ if __name__ == '__main__':
     #set up socket
     HOST = '192.168.0.64'# The remote host
     PORT = 35801 # The same port as used by the server
-    last_command = None
-    command = "flush"
     ticks = 0
+    base = 0
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #sock.connect((HOST, PORT))
-    #print sock.getsockname()
+    state = "up"
+    grab = 0
+    rover = 0
+    sock.connect((HOST, PORT))
+    print sock.getsockname()
 
     print "Initalizing the controler:", joy.get_name(), "which has", joy.get_numaxes(),"axis"
     while not rospy.is_shutdown():
         pygame.event.pump() 
-        for i in range(10):
-            print joy.get_button(i)
-        """
+        a = joy.get_button(0)
+        b = joy.get_button(1)
+        x = joy.get_button(2)
+        y = joy.get_button(3)
         turn = int(joy.get_axis(0)*127)
-        if abs(abs(vert) - abs(turn)) < 20:
-            command = "flush"
-        else:
-            if abs(vert) > abs(turn):
-                if vert < 0:
-                    command = "f10"
-                else:
-                    command = "b10"
-            elif abs(vert) < abs(turn):
-                if turn > 1:
-                    command = "r179"
-                else:
-                    command = "r181"
+        if abs(turn) > 20:
+            if turn < 0:
+                base -= 5
+                base = max(base, 0)
+            else:
+                base += 5
+                base = min(base, 50)
+        #"command,rotation,lower,upper"
+        upper = 0
+        lower = 0
+        rotation = base
+        if a:
+            state = "down"
+        elif b:
+            state = "up"
 
-        if command != last_command or ticks > 10:
-            if command != last_command:
-                send_string(sock, "flush")
-            send_string(sock, command)
-            ticks = 0
-            last_command = command
-        clock.tick(10)
-        ticks += 1
-        """
+        if x:
+            grab = 2 ^ grab
+
+        if y:
+            rover = 1 ^ rover
+
+        if state == "down":
+            upper = 80
+            lower = 80
+        else:
+            upper = 300
+            lower = 300
+
+        print "%d,%d,%d,%d" % (grab, 0, lower, upper)
+        send_string(sock, "%d,%d,%d,%d" % (grab, 0, lower, upper))
+        if a or b or x:
+            time.sleep(.5)
+        clock.tick(20)
     sock.close()
