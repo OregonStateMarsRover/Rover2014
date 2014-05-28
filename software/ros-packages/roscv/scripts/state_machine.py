b90250739a7d109bf92840daaa673dee81f75338
ldb358@gmail.com <ldb358@gmail.com>
2014-05-27 19:36:31 -0700

    added Motor sending commands from state_machine

--- software/ros-packages/roscv/scripts/state_machine.py
+++ software/ros-packages/roscv/scripts/state_machine.py
@@ -3,43 +3,75 @@ from std_msgs.msg import String
 
 
 class StateMachine(object):
-
+    
     def __init__(self, state_machine):
         self.state = "FindingObject"
         self.prevState = None
         rospy.Subscriber("Object", String, self.Object_Message)
         rospy.Subscriber("Obstacle", String, self.Obstacle_Message)
         rospy.Subscriber("Process_Management", String, self.Processes)
+	rospy.Subscriber("/motor_command/path_finding/", String, self.motor_callback)
+	rospy.Subscriber("/motor_command/find_base_station/", String, self.motor_callback)
+	rospy.Subscriber("/motor_command/object_detection/", String, self.motor_callback)
+   
+ 
 
+    def motor_callback(self, data):
+	topic = data._connection_header['topic']
+	topicStates = {'/motor_command/path_finding/' : ['AvoidObstacle','SearchPattern'], '/motor_command/find_base_station/': ['FindBaseStation'], '/motor_command/object_detection' : ['MoveTowardObject'] }
+	for state in topicStates[topic]:
+	    if state == self.state:
+		print state, topic, data.data
+		#pub.Publish(data.data) 
+    
+     
     def Object_Message(self, data):
-        if data == "Object Found":
-            if self.State == "FindingObject":
-               self.prevState = self.State
-               self.State = "MoveTowardObject"
+	if self.state == "FindingObject":
+	    if data.data == "Object Found":
+               print "Changing from FindingObject to MoveTowardObject"
+               self.prevState = self.state
+               self.state = "MoveTowardObject"
+	    else:
+		print "In FindingObject state, ignoring"
 
-        elif data == "At Object":
-            self.prevState = self.State
-            self.State = "Pickup Object"
+	elif self.state == "MoveTowardObject":
+	    if data.data == "At Object":
+		self.prevState = self.state
+		self.state = "PickupObject"
+	    else:
+		print "In MoveTowardObject state, ignoring"
 
-        elif data == "Object Picked up":
-            self.prevState = self.State
-            self.State = "Find Base Station"
+        elif self.state == "AvoidObstacle":
+	    print "In AvoidObstacle state, ignoring" 
+	
+	elif self.state == "FindBaseStation":
+	    print "In FindBaseStation state, ignoring" 	
+	
+	elif self.state == "PickupObject":
+            if data.data == "Object Retrieved":
+		self.prevState = self.state
+		self.state = "FindBaseStation"
+
+	elif self.state == "ProcessManaging":
+	    print "In ProcessManaging state, ignoring"
 
         else:
-            print "Error"
+            print "In unknown state: Error"
 
     def Obstacle_Message(self, data):
-        if data == "Blocked":
-            if self.State == "Pickup Object":
+        if data.data == "Blocked":
+            if self.state == "Pickup Object":
                 pass
             else:
-                self.prevState = self.State
-                self.State == "Avoid Obstacle"
-        elif data == "Not Blocked":
-            if self.State == "Avoid Obstacle":
-                self.State = self.prevState
+                self.prevState = self.state
+                self.state == "Avoid Obstacle"
+        elif data.data == "Not Blocked":
+            if self.state == "Avoid Obstacle":
+                self.state = self.prevState
+	    else:
+		print "Rover was not blocked, so ignoring"
         else:
-            print "Error"
+            print "Obstacle said something other than Blocked or Not Blocked: Error"
 
     def Processes(self, data):
         print "Process"
