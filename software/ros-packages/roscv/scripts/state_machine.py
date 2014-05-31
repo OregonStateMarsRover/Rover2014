a5b2f1f49ec59c250560be94c5ccd987babbe6a9
ldb358@gmail.com <ldb358@gmail.com>
2014-05-30 17:03:38 -0700

    Added flush commands when switching states

--- software/ros-packages/roscv/scripts/state_machine.py
+++ software/ros-packages/roscv/scripts/state_machine.py
@@ -40,6 +40,7 @@ class StateMachine(object):
                    print "Changing from FindingObject to MoveTowardObject"
                    self.prevState = self.state
                    self.state = "MoveTowardObject"
+		   self.motor_pub.publish("flush")
 
             elif data.data == "Object Found" and self.objectPickedup:
                 print "Object already picked up, ignoring state switch request"
@@ -49,19 +50,22 @@ class StateMachine(object):
 
             elif data.data == "Base Station Found" and self.objectPickedup:
                 print "Switching to FindBaseStation state"
+		self.motor_pub.publish("flush")
                 self.prevState = self.state
                 self.State = "FindBaseStation"
+
 
             elif data.data == "Base Station Found Final" and not(self.objectPickedup):
                 print "Object not picked up, ignoring request to change state to FindBaseStationFinal"
 
             elif data.data == "Base Station Found Final" and self.objectPickedup:
-                print "Switching to FindBasestationFinal state"
+                print "Switching to FindBaseStationFinal state"
                 self.prevState = self.state
                 self.State = "FindBaseStationFinal"
 
             elif data.data == "Blocked":
                 print "Switching to AvoidObstacle state"
+		self.motor_pub.publish("flush")
                 self.prevState = self.state
                 self.state = "AvoidObstacle"
 
@@ -73,10 +77,12 @@ class StateMachine(object):
                 print "Changing from MoveTowardObject to PickupObject state"
                 self.prevState = self.state
                 self.state = "PickupObject"
+		self.motor_pub.publish("flush")
 
             elif data.data == "Blocked":
                 print "Changing frm MoveTowardObject to AvoidObstacle state"
-                self.prevState = self.state
+                self.motor_pub.publish("flush")
+		self.prevState = self.state
                 self.state = "AvoidObstacle"
 
             else:
@@ -85,6 +91,7 @@ class StateMachine(object):
         elif self.state == "AvoidObstacle":
             if data.data == "Not Blocked":
                 print "Changing from  AvoidObstacle to prevstate: %s" % self.prevState
+                self.motor_pub.publish("flush")
                 self.state = self.prevState
                 self.prevState = "AvoidObstacle"
 
@@ -94,6 +101,7 @@ class StateMachine(object):
         elif self.state == "FindBaseStation":
             if data.data == "Blocked":
                 print "Changing from FindBaseStation to AvoidObstacle state"
+                self.motor_pub.publish("flush")
                 self.prevState = self.state
                 self.state = "AvoidObstacle"
 
@@ -112,6 +120,7 @@ class StateMachine(object):
         elif self.state == "PickupObject":
                 if data.data == "Object Retrieved":
                     print "Object retrieved"
+		    self.motor_pub.publish("flush")
                     print "Changing from PickupObject to SearchPattern"
                     self.prevState = self.state
                     self.objectPickedup = True
