ab9715a28c2a0cdb0c0dade59089a271087d4e9b
ldb358 <ldb358@gmail.com>
2014-06-01 00:49:41 -0700

    finished arm state machine

--- software/ros-packages/roscv/scripts/ArmStates.py
+++ software/ros-packages/roscv/scripts/ArmStates.py
@@ -19,17 +19,21 @@ class ArmState(object):
         self.state = "home"
         self.item_count = 0
         self.arm = rospy.Publisher("arm_commands", std_msgs.msg.String)
+        self.arm_state = rospy.Subscriber("/arm_state", std_msgs.msg.String, self.change_state)
 
     def change_state(self, state):
+        try:
+            nxt = self.states[state]()
+        except KeyError:
+            return
         prev = self.states[self.state]()
-        next = self.states[state]()
         self.move(prev["from"])
-        self.move(next["to"])
+        self.move(nxt["to"])
 
     def move(self, commands):
         for l in commands:
-            self.arm.publish("%d, %d, %d, %d" % l)
             self.wait_for_arm()
+            self.arm.publish("%d, %d, %d, %d" % l)
 
     def wait_for_arm(self):
         while rospy.wait_for_message("/arm_controller", std_msgs.msg.String).data == "Not":
@@ -51,13 +55,13 @@ class ArmState(object):
     #TODO find the grab position
     def grab(self):
         position = [(0, 0, 0, 300), (2, 0, 0, 240)]
-        back = [(2, 0, 300, 350)]
-        return self.move_home()
+        back = [(2, 0, 350, 300)]
+        return {"to": position, "from": back}
 
     def store(self):
         place = self.item_count
         #position will be a list lists where each list is the instructions to get to the next pocket
-        position = [[(), ()], [(), ()]]
+        position = [[(6, 0, 350, 300), (2, 185, 280, 325)]]
         #return will lift then rotate to home
         back = [[(), ()], [(), ()]]
         return self.move_home()
@@ -69,10 +73,8 @@ if __name__ == '__main__':
     try:
         rospy.init_node("Arm State Machine", anonymous=True)
         #handle lethal signals in order to stop the motors if the script quits
-        start = FindStart()
+        start = ArmState()
         #start.points(rospy.wait_for_message("/my_stereo/disparity", stereo_msgs.msg.DisparityImage))
-        while True:
-            #start.image(rospy.wait_for_message("/my_stereo/left/image_rect_color", sensor_msgs.msg.Image))
-            start.image()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
