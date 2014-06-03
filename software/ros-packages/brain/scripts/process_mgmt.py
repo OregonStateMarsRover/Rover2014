178e32042e9de66900f4161fdf533a10f1af6694
Erich Merrill <merriler@engr.oregonstate.edu>
2014-06-03 16:21:27 -0700

    Modified process manager to be even *more* general-purpose!

--- software/ros-packages/brain/scripts/process_mgmt.py
+++ software/ros-packages/brain/scripts/process_mgmt.py
@@ -11,6 +11,13 @@ import sys
 from std_msgs.msg import String
 from subprocess import Popen, PIPE
 
+PROCESS_ORDER = ("camera", "stereo", "obstacle", "pathfinding")
+PROCESS_ARGS = {"camera" : ['roslaunch', 'roscv', 'startCam.launch'],
+"stereo": ['roslaunch', 'roscv', 'startStereo.launch'],
+"obstacle": ['rosrun', 'roscv2', 'obstacle_detect'],
+"pathfinding": ['rosrun', 'roscv2', 'pathfinding']
+}
+
 class ProcessManager:
 	def __init__(self):
 		def handler(sig, f):
@@ -19,18 +26,19 @@ class ProcessManager:
 			sys.exit(0)
 
 		self.processes = {}
+		for p in PROCESS_ORDER:
+			self.processes[p] = Process(PROCESS_ARGS[p], p)
+
 		signal.signal(signal.SIGINT, handler)
 		rospy.Subscriber("process_mgmt", String, lambda data: self.callback(data))
 
 	def callback(self, data):
 		command, arg = data.data.split()
 		if command == "start":
-			if arg == "stereo":
-				self.start_stereo()
-			elif arg == "all":
+			if arg == "all":
 				self.start_all()
 			else:
-				self.start_normal(arg)
+				self.start_process(arg)
 		elif command == "stop":
 			if arg == "all":
 				self.stop_all()
@@ -41,63 +49,36 @@ class ProcessManager:
 				self.restart_all()
 			else:
 				self.restart_process(arg)
-		elif command == "remove":
-			self.remove(arg)
 
 	def start_all(self):
-		self.start_stereo()
-		self.start_obstacle()
+		for p in PROCESS_ORDER:
+			self.start_process(p)
 
 	def stop_all(self):
-		for p in self.processes.values():
-			if p.running():
-				p.stop()
-			else:
-				print p.running()
-				print p.proc.poll()
+		for p in reversed(PROCESS_ORDER):
+			self.stop_process(p)
 	
 	def restart_all(self):
-		for p in self.processes.values():
-			p.restart()
+		self.stop_all()
+		self.start_all()
 
-	def start_process(self, args, name):
-		if not name in self.processes:
-			proc = Process(args, name)
-			self.processes[name] = proc
-		self.processes[name].start()
+	def start_process(self, name):
+		if name in self.processes:
+			self.processes[name].start()
+		else:
+			rospy.loginfo("Error - Can't start process `%s` - not present")
 
 	def stop_process(self, name):
 		if name in self.processes:
 			self.processes[name].stop()
+		else:
+			rospy.loginfo("Error - Can't stop process `%s` - not present")
 
 	def restart_process(self, name):
 		if name in self.processes:
 			self.processes[name].restart()
-
-	def remove(self, name):
-		if name in self.processes:
-			proc = self.processes[name]
-			if proc.running():
-				proc.stop()
-			del self.processes[name]
-
-	def start_stereo(self):
-		proc = StereoProcess()
-		self.processes['stereo'] = proc
-		proc.start()
-
-	def start_normal(self, name):
-		if name == "obstacle":
-			args = ['rosrun', 'roscv2', 'obstacle_detect']
-		self.start_process(args, name)
-
-
-"""
-	def start_pathfinding(self):
-		move_args = ['rosrun', 'roscv', 'pathfinding']
-		self.move_proc = Popen(move_args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		rospy.loginfo("Started pathfinding")
-"""
+		else:
+			rospy.loginfo("Error - Can't restart process `%s` - not present")
 
 class Process:
 	def __init__(self, args, name=None, keep_alive=False):
@@ -130,33 +111,8 @@ class Process:
 		running = started and self.proc.poll() is True
 		return running
 
-class StereoProcess:
-	def __init__(self):
-		cam_args = ['roslaunch', 'roscv', 'startCam.launch']
-		stereo_args = ['roslaunch', 'roscv', 'startStereo.launch']
-		self.cam_proc = Process(cam_args, 'camera')
-		self.stereo_proc = Process(stereo_args, 'stereo')
-
-	def start(self):
-		self.cam_proc.start()
-		self.stereo_proc.start()
-
-	def stop(self):
-		if self.stereo_proc.running():
-			self.stereo_proc.stop()
-		if self.cam_proc.running():
-			self.cam_proc.stop()
-
-	def restart(self):
-		self.cam_proc.restart()
-		self.stereo_proc.restart()
-
-	def running(self):
-		cam_running = self.cam_proc.running()
-		stereo_running = self.stereo_proc.running()
-		return cam_running and stereo_running
-
 if __name__=="__main__":
 	rospy.init_node("process_manage")
 	proc = ProcessManager()
+	proc.start_all()
 	rospy.spin()
